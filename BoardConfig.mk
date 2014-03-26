6f5a8b4193c2bef592c99f6eaf20777507cf6a86
Martin Rodemann <martin@rodemann.org>
2014-03-26 11:53:54 +0100

    Mako optimizations (credits to ctfrommn)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,17 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
+# Flags for Krait CPU
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_ARCH_VARIANT_FPU := neon-vfpv4
+TARGET_EXTRA_CFLAGS := -mtune=cortex-a15
+
+# Build thumb modules at optimization level 3
+# with LLVM Polly optimizations.
+ARCH_ARM_HIGH_OPTIMIZATION := true
+
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 TARGET_USE_KRAIT_PLD_SET := true
@@ -37,8 +48,10 @@ TARGET_USE_O3 := true
 # Enable various prefetch optimizations
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
-# Compiler Optimization
-ARCH_ARM_HIGH_OPTIMIZATION := true
+# Boot animation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
 
 TARGET_NO_BOOTLOADER := true
 
